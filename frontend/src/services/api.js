012b5da02d8c1ac66303de6026ae7b5e5bbb37fb
mayurthek <zusokvlog@gmail.com>
2026-03-07 05:20:43 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
